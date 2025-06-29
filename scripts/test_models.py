f2d80d2f3eca86fb4eab3516f25aadf03933ad2d
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2025-06-29 18:38:08 -0400

    Update test_models.py

--- scripts/test_models.py
+++ scripts/test_models.py
@@ -8,6 +8,7 @@ from model_1 import model_1
 from model_2 import model_2
 
 model_names = [model_1, model_2]
+config_args = yaml.safe_load(configs/model_config.yaml)
 
 for model_name in model_names:
     model = model_1(train, test, config_args[model_name])
